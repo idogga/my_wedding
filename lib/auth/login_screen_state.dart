2fad0c39eb0a6d4c7fbe4e27029843db007a16c6
Artyom <Lenko>
2020-09-06 10:42:06 +0700

    #ios ПОдготовка к запуску

--- lib/auth/login_screen_state.dart
+++ lib/auth/login_screen_state.dart
@@ -56,6 +56,8 @@ class LoginScreenState extends State<LoginScreen> {
     this.setState(() {
       isLoading = true;
     });
+try {
+
 
     GoogleSignInAccount googleUser = await googleSignIn.signIn();
 
@@ -110,9 +112,16 @@ class LoginScreenState extends State<LoginScreen> {
       });
     }
   }
+  
+  catch (e){
+_showToast(e);
+  }
+  }
 
   _showToast(String text) {
-    Widget toast = Container(
+    print(text);
+    
+    var toast = Container(
       padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(25.0),
